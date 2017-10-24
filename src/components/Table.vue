d7d2e9df6d157350ee9ca8524c16d3c4668b247f
Siwa Khongsuphap <siwa@hospital-os.com>
2017-10-24 18:00:07 +0700

    add table example

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,5 +1,32 @@
 <template>
-  <h1 class="heading">{{ msg }}</h1>
+  <div>
+    <h1 class="heading">{{ msg }}</h1>
+    <div class="example">
+      <div class="example__content">
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Lastname</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Adam</td>
+              <td>Smith</td>
+            </tr>
+            <tr>
+              <td>Adam</td>
+              <td>Smith</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div class="example__code">
+        <pre v-highlightjs="sourcecode"><code class="xml"></code></pre>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,8 +34,39 @@ export default {
   name: 'Table',
   data () {
     return {
-      msg: 'Table Page'
+      msg: 'Table Page',
+      sourcecode: `
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Lastname</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Adam</td>
+            <td>Smith</td>
+          </tr>
+          <tr>
+            <td>Adam</td>
+            <td>Smith</td>
+          </tr>
+        </tbody>
+      </table>
+      `
     }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+  table {
+    border-collapse: collapse;
+
+    td, th {
+      padding: 5px;
+      border: 1px solid #ccc;
+    }
+  }
+</style>
